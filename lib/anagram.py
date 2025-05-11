168163844249a8bd15f7c0e8735fca71c51825d5
aminacherop <cheropamina26@gmail.com>
2025-05-11 17:14:17 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for words in word_list:
+            lower_words = words.lower()
+            if lower_words!= self.word and sorted(lower_words) == self.sorted_word:
+                matches.append(words)
+        return matches
+
+anagram = Anagram("enlist")
+print(anagram.match(["listen", "silent", "hippopotamus"]))  
